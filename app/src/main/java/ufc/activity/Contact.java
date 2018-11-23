0afdfce3d779a253efe2412789286073736b7731
AttilaNagy96 <attila.kidd@gmail.com>
2018-11-23 18:50:42 +0000

    UFC Application CA2

--- app/src/main/java/ufc/activity/Contact.java
+++ app/src/main/java/ufc/activity/Contact.java
@@ -6,13 +6,50 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Contact extends AppCompatActivity {
+
+    EditText mailTo,mailSubject,mailMessage;
+    Button sendEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+
+        mailTo = (EditText)findViewById(R.id.editText);
+        mailSubject = (EditText)findViewById(R.id.editText2);
+        mailMessage = (EditText)findViewById(R.id.editText3);
+
+        sendEmail = (Button)findViewById(R.id.sendEmail);
+        sendEmail.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+
+                //Get and Set editText value in String.
+                String to = mailTo.getText().toString();
+                String subject = mailSubject.getText().toString();
+                String message = mailMessage.getText().toString();
+
+                Intent email = new Intent(Intent.ACTION_SEND);
+                email.putExtra(Intent.EXTRA_EMAIL, new String[]{ to});
+                email.putExtra(Intent.EXTRA_SUBJECT, subject);
+                email.putExtra(Intent.EXTRA_TEXT, message);
+
+                //This will show prompts of email intent
+                email.setType("message/rfc822");
+                startActivity(Intent.createChooser(email, "Choose an Email sender :"));
+            }
+
+        });
     }
 
     @Override
